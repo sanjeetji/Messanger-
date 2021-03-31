728dba736874eb44f30d8ccfb2a6d1ff3c9e36d7
sanjeetji <sk698166@gmail.com>
2021-03-31 16:47:22 +0530

    Using Firebase

---
+++ app/src/main/java/com/sanjeet/chatapp/Fragments/APIService.java
@@ -0,0 +1,22 @@
+package com.sanjeet.chatapp.Fragments;
+
+
+import com.sanjeet.chatapp.Notifications.MyResponse;
+import com.sanjeet.chatapp.Notifications.Sender;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.Headers;
+import retrofit2.http.POST;
+
+public interface APIService {
+    @Headers(
+            {
+                    "Content-Type:application/json",
+                    "Authorization:key=AAAAxNjVIwY:APA91bGFerYPN56D0NJRXKUK1eOgij3Yb_N0XuBgs4efT2b4U9Qdq2hqbpHloHIjMNhZxhaWaMv8k9n-kAGff5TndZnkk1jj3PYGLbmt6jsuWJoXbM8VFIGRj-EcmnJCQfQpriSrMsTN"
+            }
+    )
+
+    @POST("fcm/send")
+    Call<MyResponse> sendNotification(@Body Sender body);
+}
---
+++ app/src/main/java/com/sanjeet/chatapp/Notifications/MyResponse.java
@@ -0,0 +1,6 @@
+package com.sanjeet.chatapp.Notifications;
+
+public class MyResponse {
+
+    public int success;
+}
